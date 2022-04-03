6b662049465f8707a201879be58ac4026c4f52b3
Giorgos Livanos <livanos.g@gmail.com>
2022-04-03 04:19:56 +0300

    define class_weights depending on loss function add LearningRateScheduler and warmup for the first 5 epochs

--- train_script.py
+++ train_script.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 import tensorflow_addons as tfa
 import models
+from data_pipe import MelData
 from metrics import calc_metrics
 from callbacks import LaterCheckpoint, EnrTensorboard
-from custom_losses import binary_focal_loss, PerClassWeightedCategoricalCrossentropy, custom_loss, WeightedCategoricalCrossentropy
+from custom_losses import categorical_focal_loss, combined_loss, CMWeightedCategoricalCrossentropy
 
 
 def unfreeze_model(trained_model):
@@ -32,41 +33,57 @@ def setup_model(args):
     return model, strategy
 
 
-def train_fn(args, data):
+def train_fn(args):
     model, strategy = setup_model(args)
+    data = MelData(args=args)
+    class_weight = data.get_class_weights()
+    custom_loss_class_weights = None
+    if args['loss_fn'] in ('focal', 'combined'):
+        custom_loss_class_weights = []
+        for i in class_weight.keys():
+            custom_loss_class_weights.append(class_weight[i])
+        class_weight = None
+    args['learning_rate'] = args['learning_rate'] * strategy.num_replicas_in_sync
+    args['batch_size'] = args['batch_size'] * strategy.num_replicas_in_sync
 
-    loss_fn = {'cxe': 'categorical_crossentropy', 'focal': binary_focal_loss(), 'custom': custom_loss(args['loss_frac']),
-               'perclass': PerClassWeightedCategoricalCrossentropy(args=args),
-               'wcxe': WeightedCategoricalCrossentropy(args=args)}[args['loss_fn']]
-    optimizer = {'adam': tf.keras.optimizers.Adam, 'adamax': tf.keras.optimizers.Adamax, 'nadam': tf.keras.optimizers.Nadam,
-                 'ftrl': tf.keras.optimizers.Ftrl, 'rmsprop': tf.keras.optimizers.RMSprop, 'sgd': tf.keras.optimizers.SGD,
-                 'adagrad': tf.keras.optimizers.Adagrad, 'adadelta': tf.keras.optimizers.Adadelta}[args['optimizer']]
+    loss_fn = {'cxe': 'categorical_crossentropy', 'focal': categorical_focal_loss(alpha=[0.25, 0.25], weights=custom_loss_class_weights),
+               'combined': combined_loss(args['loss_frac'], weights=custom_loss_class_weights),  #
+               'perclass': CMWeightedCategoricalCrossentropy(args=args)}[args['loss_fn']]
 
-    lr = args['learning_rate'] * strategy.num_replicas_in_sync
+
     with strategy.scope():
-        model.compile(optimizer=optimizer(learning_rate=lr), loss=loss_fn, metrics=[tfa.metrics.F1Score(num_classes=args['num_classes'], average='macro')])
+        optimizer = {'adam': tf.keras.optimizers.Adam, 'adamax': tf.keras.optimizers.Adamax,
+                     'nadam': tf.keras.optimizers.Nadam, 'ftrl': tf.keras.optimizers.Ftrl,
+                     'rmsprop': tf.keras.optimizers.RMSprop, 'sgd': tf.keras.optimizers.SGD,
+                     'adagrad': tf.keras.optimizers.Adagrad, 'adadelta': tf.keras.optimizers.Adadelta}[args['optimizer']]
+        model.compile(loss=loss_fn,
+                      optimizer=optimizer(learning_rate=args['learning_rate']),
+                      metrics=[tfa.metrics.F1Score(num_classes=args['num_classes'], average='macro')])
 
     with open(args['dir_dict']['model_summary'], 'w') as f:
         model.summary(print_fn=lambda x: f.write(x + '\n'))
-    start_save = 10
-    rop_patience = 10
+    start_save = 1000
+    rop_patience = 20
     if args['fine']:
         rop_patience = 5
-    es_patience = rop_patience * 2
+    es_patience = 3 * rop_patience
 
-    args['batch_size'] = args['batch_size'] * strategy.num_replicas_in_sync
     train_data = data.get_dataset(mode='train')
     val_data = data.get_dataset(mode='validation')
-    callbacks = [tf.keras.callbacks.ReduceLROnPlateau(factor=0.1, patience=rop_patience),
+    callbacks = [tf.keras.callbacks.LearningRateScheduler(schedule=lambda epoch, lr: args['learning_rate'] * 10 if epoch < 5 else args['learning_rate']),  # warmup model
+                 tf.keras.callbacks.ReduceLROnPlateau(factor=0.1, patience=rop_patience),
                  tf.keras.callbacks.EarlyStopping(patience=es_patience),
                  tf.keras.callbacks.CSVLogger(filename=args['dir_dict']['train_logs'], separator=',', append=True),
                  LaterCheckpoint(filepath=args['dir_dict']['save_path'], save_best_only=True, start_at=start_save),
                  EnrTensorboard(val_data=val_data, log_dir=args['dir_dict']['logs'], class_names=args['class_names'])]
-    model.fit(x=train_data, validation_data=val_data, callbacks=callbacks, epochs=args['epochs'])  # steps_per_epoch=np.floor(data.train_len / batch),
+    model.fit(x=train_data, validation_data=val_data,
+              class_weight=class_weight,  # bugged
+              callbacks=callbacks, epochs=args['epochs'])  # steps_per_epoch=np.floor(data.train_len / batch),
 
 
-def test_fn(args, data):
+def test_fn(args):
     model, strategy = setup_model(args)
+    data = MelData(args=args)
     thresh_dist, thresh_f1 = calc_metrics(args=args, model=model, dataset=data.get_dataset(mode='validation',), dataset_type='validation')
     #thresh_dist, thresh_f1 = None, None
     if args['image_type'] in ('both', 'derm'):
