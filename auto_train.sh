939a121f389d79606acf3035782e66b00a5f72bd
Giorgos Livanos <livanos.g@gmail.com>
2022-04-04 08:35:05 +0300

    move class weight to train dataset pipeline write g-mean sensitivity and specificity def

--- auto_train.sh
+++ auto_train.sh
@@ -1,10 +1,12 @@
-python main.py -pt effnet6 -task ben_mal -it clinic -is 500 -btch 64 -io -loss focal
-python main.py -pt effnet6 -task ben_mal -it both -is 500 -btch 64 -io -loss focal
-python main.py -pt effnet6 -task nev_mel -it clinic -is 500 -btch 64 -io -loss focal
-python main.py -pt effnet6 -task nev_mel -it both -is 500 -btch 64 -io -loss focal
-python main.py -pt effnet6 -task ben_mal -it clinic -is 500 -btch 64 -nit -loss focal
-python main.py -pt effnet6 -task ben_mal -it both -is 500 -btch 64 -loss focal
-python main.py -pt effnet6 -task nev_mel -it clinic -is 500 -btch 64 -nit -loss focal
-python main.py -pt effnet6 -task nev_mel -it both -is 500 -btch 64 -loss focal
-python main.py -pt effnet6 -task ben_mal -it both -is 500 -btch 64 -nit -loss focal
-python main.py -pt effnet6 -task nev_mel -it both -is 500 -btch 64 -nit -loss focal
+python main.py -pt effnet6 -task nev_mel -it clinic -is 224 -btch 64 -opt adamax -ncd -loss focal -clrs 128 -mlrs 128 -dor 0.2 -lr 1e-5 -strg mirrored -gpus 2 # 3
+python main.py -pt effnet6 -task nev_mel -it clinic -is 224 -btch 64 -opt adamax -ncd -loss focal -wl -clrs 128 -mlrs 128 -dor 0.2 -lr 1e-5 -strg mirrored -gpus 2 # 3
+#python main.py -pt effnet6 -task nev_mel -it both -is 224 -btch 64 -opt adamax -ws -loss focal -clrs 128 -mlrs 128 -dor 0.3 -lr 1e-5 -strg mirrored -gpus 2 # 3
+#python main.py -pt effnet6 -task nev_mel -it both -is 224 -btch 64 -ncd -opt adam -loss focal -clrs 128 -mlrs 128 -dor 0.3 -lr 1e-4 -strg mirrored -gpus 2 # 4
+#python main.py -pt effnet6 -task ben_mal -it clinic -is 224 -btch 64 -ncd -opt adam -loss focal -lossf 1. -clrs 128 -mlrs 128 -dor 0.3 -lr 1e-5 -strg mirrored -gpus 2 # 1
+#python main.py -pt effnet6 -task ben_mal -it both -is 224 -btch 64 -ncd -opt adam -loss focal -clrs 128 -mlrs 128 -dor 0.3 -lr 1e-5 -strg mirrored -gpus 2 # 2
+#python main.py -pt effnet6 -task ben_mal -it clinic -is 224 -btch 64 -nit -opt adam -loss focal -clrs 128 -dlrs 64 -mlrs 128 -dor 0.3 -lr 1e-4 -strg mirrored -gpus 2 # 5
+#python main.py -pt effnet6 -task ben_mal -it both -is 224 -btch 64 -opt adam -loss focal -clrs 128 -dlrs 64 -mlrs 128 -dor 0.3 -lr 1e-4 -strg mirrored -gpus 2 # 6
+#python main.py -pt effnet6 -task nev_mel -it clinic -is 224 -btch 64 -nit -opt adam -loss focal -clrs 128 -dlrs 64 -mlrs 128 -dor 0.3 -lr 1e-4 -strg mirrored -gpus 2 # 7
+#python main.py -pt effnet6 -task nev_mel -it both -is 224 -btch 64 -opt adam -loss focal -clrs 128 -dlrs 64 -mlrs 128 -dor 0.3 -lr 1e-4 -strg mirrored -gpus 2 # 8
+#python main.py -pt effnet6 -task ben_mal -it both -is 224 -btch 64 -nit -opt adam -loss focal -clrs 128 -dlrs 64 -mlrs 128 -dor 0.3 -lr 1e-4 -strg mirrored -gpus 2 # 9
+#python main.py -pt effnet6 -task nev_mel -it both -is 224 -btch 64 -nit -opt adam -loss focal -clrs 128 -dlrs 64 -mlrs 128 -dor 0.3 -lr 1e-4 -strg mirrored -gpus 2 # 10
