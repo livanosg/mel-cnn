5ea6bf62303fa93c616c6ceab04c204356343ee2
Giorgos Livanos <livanos.g@gmail.com>
2022-04-04 08:30:49 +0300

    remove weight from loss_fn

--- custom_losses.py
+++ custom_losses.py
@@ -30,7 +30,7 @@ class CMWeightedCategoricalCrossentropy(tf.keras.losses.CategoricalCrossentropy)
 
 
 # categorical focal loss from https://github.com/umbertogriffo/focal-loss-keras/blob/master/src/loss_function/losses.py
-def categorical_focal_loss(alpha, gamma=2., weights=None):
+def categorical_focal_loss(alpha, gamma=2.):
     """
     Softmax version of focal loss.
     When there is a skew between different categories/labels in your data set, you can try to apply this function as a
@@ -53,46 +53,35 @@ def categorical_focal_loss(alpha, gamma=2., weights=None):
      model.compile(loss=[categorical_focal_loss(alpha=[[.25, .25, .25]], gamma=2)], metrics=["accuracy"], optimizer=adam)
     """
     alpha = tf.constant(alpha, dtype=tf.float32)
-    class_weights = weights
+
     def categorical_focal_loss_fixed(y_true, y_pred):
         """
         :param y_true: A tensor of the same shape as `y_pred`
         :param y_pred: A tensor resulting from a softmax
         :return: Output tensor.
         """
-        if class_weights is None:
-            weights = tf.ones_like(y_true)
-        else:
-            # weights = class_weights
-            weights = tf.ones_like(y_true)
-
         # Clip the prediction value to prevent NaN's and Inf's
         epsilon = 1e-7
         y_pred = tf.clip_by_value(y_pred, epsilon, 1. - epsilon)
         # Calculate Cross Entropy
         cross_entropy = tf.math.multiply(x=tf.math.multiply(x=-1., y=y_true), y=tf.math.log(y_pred))
         # Calculate Focal Loss
-        loss = tf.reduce_sum(tf.math.multiply(x=weights, y=alpha * tf.math.pow(1 - y_pred, gamma) * cross_entropy), axis=-1)
-        # Compute sample sum loss
-        # print(loss.shape)
-        return loss   # tf.math.reduce_mean(tf.math.reduce_sum())
+        loss = alpha * tf.math.pow(1 - y_pred, gamma) * cross_entropy
+        return tf.reduce_sum(loss, axis=-1)
     return categorical_focal_loss_fixed
 
 
-def combined_loss(frac, weights=None):
+def combined_loss(frac):
     def total_loss(y_true, y_pred):
-        nonlocal weights
         """Inputs: y_pred: probs form per class
                    y_true: one-hot encoding of label
         """
-        if weights is None:
-            weights = tf.ones_like(y_true)
         with tf.name_scope('Generalized_Dice_Log_Loss'):
-            numerator = tf.reduce_sum(input_tensor=tf.math.multiply(x=weights * y_true, y=y_pred), axis=-1)
+            numerator = tf.reduce_sum(input_tensor=tf.math.multiply(x=y_true, y=y_pred), axis=-1)
             denominator = tf.reduce_sum(input_tensor=tf.math.add(x=y_true, y=y_pred), axis=-1)
             dice = tf.math.divide(x=tf.math.multiply(x=2., y=numerator), y=denominator)
             dice_loss = -tf.math.log(x=dice)
         with tf.name_scope('Crossentropy_Loss'):
-            cxe_loss = tf.keras.losses.categorical_crossentropy(y_true=tf.multiply(x=weights, y=y_true), y_pred=y_pred)
+            cxe_loss = tf.keras.losses.categorical_crossentropy(y_true=y_true, y_pred=y_pred)
         return tf.add(x=tf.math.multiply(x=frac, y=dice_loss), y=tf.math.multiply(x=tf.math.subtract(x=1., y=frac), y=cxe_loss))
     return total_loss
