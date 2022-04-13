1dc12415b82c36e7bc4b526fb7156de3715dfdf7
Giorgos Livanos <livanos.g@gmail.com>
2022-04-13 08:56:40 +0300

    apply Directories get read_image out of class

--- data_pipe.py
+++ data_pipe.py
@@ -2,34 +2,28 @@ import os
 import pandas as pd
 import tensorflow as tf
 import tensorflow_addons as tfa
-from config import BEN_MAL_MAP, LOCATIONS, IMAGE_TYPE, SEX, AGE_APPROX,\
-    TASK_CLASSES, MAIN_DIR, ISIC20_TEST_PATH, TEST_CSV_PATH, VAL_CSV_PATH,\
-    TRAIN_CSV_PATH, ISIC16_TEST_PATH, ISIC17_TEST_PATH, ISIC18_VAL_TEST_PATH,\
-    DERMOFIT_TEST_PATH, UP_TEST_PATH, MCLASS_CLINIC_TEST_PATH, MCLASS_DERM_TEST_PATH
+from features import BEN_MAL_MAP, LOCATIONS, IMAGE_TYPE, SEX, AGE_APPROX, TASK_CLASSES
+from settings import data_csv
+
+
+def read_image(sample):
+    sample['image'] = tf.cast(x=tf.io.decode_image(tf.io.read_file(sample['image_path']), channels=3),
+                              dtype=tf.float32)
+    return sample
 
 
 class MelData:
-    def __init__(self, args):
+    def __init__(self, args, dirs):
         self.args = args
-        self.tf_rng = tf.random.Generator.from_non_deterministic_state()  # .from_seed(1312)
-        self.class_names = TASK_CLASSES[args['task']]
-        self.num_classes = len(TASK_CLASSES[args['task']])
+        self.dirs = dirs
+        self.rng = tf.random.Generator.from_non_deterministic_state()  # .from_seed(1312)
+        self.class_names = TASK_CLASSES[self.args['task']]
 
     def prep_df(self, mode: str):
-        df = pd.read_csv({'train': TRAIN_CSV_PATH,
-                          'validation': VAL_CSV_PATH,
-                          'test': TEST_CSV_PATH,
-                          'isic16_test': ISIC16_TEST_PATH,
-                          'isic17_test': ISIC17_TEST_PATH,
-                          'isic18_val_test': ISIC18_VAL_TEST_PATH,
-                          'isic20_test': ISIC20_TEST_PATH,
-                          'dermofit_test': DERMOFIT_TEST_PATH,
-                          'up_test': UP_TEST_PATH,
-                          'mclass_clinic_test': MCLASS_CLINIC_TEST_PATH,
-                          'mclass_derm_test': MCLASS_DERM_TEST_PATH}[mode])
+        df = pd.read_csv(data_csv[mode])
         if mode in ('train', 'validation'):
             df = df.sample(frac=self.args['dataset_frac'], random_state=1312)
-        df['image'] = df['image'].apply(lambda x: os.path.join(self.args['dir_dict']['data_folder'], x))
+        df['image'] = df['image'].apply(lambda x: os.path.join(self.dirs['proc_img_folder'], x))
         df['location'].fillna('', inplace=True)
         df['sex'].fillna('', inplace=True)
         df['age_approx'].fillna(-1, inplace=True)
@@ -48,11 +42,15 @@ class MelData:
             df.drop(df[~df['image_type'].isin([self.args['image_type']])].index, errors='ignore', inplace=True)
         # log datasets
         if mode in ('train', 'validation', 'test'):
-            os.makedirs(os.path.join(MAIN_DIR, 'data_info'), exist_ok=True)
-            with pd.ExcelWriter(os.path.join(MAIN_DIR, 'data_info', 'descr_{}_{}_{}.xlsx'.format(self.args['task'], self.args['image_type'], mode)), mode='w') as writer:
+            os.makedirs(self.dirs['data_info'], exist_ok=True)
+            desc_path = os.path.join(self.dirs['data_info'],
+                                     'descr_{}_{}_{}.xlsx'.format(self.args['task'], self.args['image_type'], mode))
+            with pd.ExcelWriter(desc_path, mode='w') as writer:
                 for feature in ['sex', 'age_approx', 'location']:
-                    logs_df = df[['image_type', 'class', feature]].value_counts(sort=False, dropna=False).to_frame('counts')
-                    logs_df = logs_df.pivot_table(values='counts', fill_value=0, index=['image_type', feature], columns='class', aggfunc=sum)
+                    logs_df = df[['image_type', 'class', feature]].value_counts(sort=False,
+                                                                                dropna=False).to_frame('counts')
+                    logs_df = logs_df.pivot_table(values='counts', fill_value=0,
+                                                  index=['image_type', feature], columns='class', aggfunc=sum)
                     logs_df = logs_df[self.class_names]
                     logs_df.to_excel(writer, sheet_name=feature)
 
@@ -69,9 +67,12 @@ class MelData:
         onehot_input['image_type'] = img_type_lookup(tf.convert_to_tensor(df['image_type'].values))[:, 1:]
 
         if not self.args['no_clinical_data']:
-            onehot_input['clinical_data'] = tf.keras.layers.Concatenate()([onehot_input['location'], onehot_input['sex'], onehot_input['age_approx']])
+            onehot_input['clinical_data'] = tf.keras.layers.Concatenate()([onehot_input['location'],
+                                                                           onehot_input['sex'],
+                                                                           onehot_input['age_approx']])
             if not self.args['no_image_type']:
-                onehot_input['clinical_data'] = tf.keras.layers.Concatenate()([onehot_input['clinical_data'], onehot_input['image_type']])
+                onehot_input['clinical_data'] = tf.keras.layers.Concatenate()([onehot_input['clinical_data'],
+                                                                               onehot_input['image_type']])
 
         onehot_label = None
         if mode != 'isic20_test':
@@ -89,7 +90,7 @@ class MelData:
                 sample_weights = tf.gather(sample_weights, tf.math.argmax(onehot_input['image_type'], axis=-1))
             if self.args['weighted_loss']:  # Class weight
                 # class_weights = tf.math.divide(tf.reduce_sum(onehot_label['class']),
-                #                                tf.math.multiply(tf.cast(self.num_classes, dtype=tf.float32),
+                #                                tf.math.multiply(tf.cast(len(self.class_names), dtype=tf.float32),
                 #                                                 tf.reduce_sum(onehot_label['class'], axis=0)))
                 samples_per_class = tf.reduce_sum(onehot_label['class'], axis=0)
                 class_weights = tf.math.divide(tf.reduce_max(samples_per_class), samples_per_class)
@@ -109,24 +110,27 @@ class MelData:
         dataset = tf.data.Dataset.from_tensor_slices(data)
         if dataset_name == 'train':
             dataset = dataset.shuffle(buffer_size=dataset.cardinality(), reshuffle_each_iteration=True)
-        dataset = dataset.map(lambda sample, label, sample_weights: (self.read_image(sample=sample), label, sample_weights), num_parallel_calls=tf.data.AUTOTUNE)
-        dataset = dataset.batch(self.args['batch_size'])
+        dataset = dataset.map(lambda sample, label, sample_weights:
+                              (read_image(sample=sample), label, sample_weights),
+                              num_parallel_calls=tf.data.AUTOTUNE)
+        dataset = dataset.batch(self.args['batch_size'] * self.args['gpus'])
         if dataset_name == 'train':
-            dataset = dataset.map(lambda sample, label, sample_weights: (self.augm(sample), label, sample_weights), num_parallel_calls=tf.data.AUTOTUNE)
+            dataset = dataset.map(lambda sample, label, sample_weights:
+                                  (self.augm(sample), label, sample_weights),
+                                  num_parallel_calls=tf.data.AUTOTUNE)
         elif dataset_name == 'isic20_test':
-            dataset = dataset.map(lambda sample, label, sample_weights: sample, num_parallel_calls=tf.data.AUTOTUNE)
+            dataset = dataset.map(lambda sample, label, sample_weights: sample,
+                                  num_parallel_calls=tf.data.AUTOTUNE)
         else:
-            dataset = dataset.map(lambda sample, label, sample_weights: (sample, label), num_parallel_calls=tf.data.AUTOTUNE)
+            dataset = dataset.map(lambda sample, label, sample_weights:
+                                  (sample, label),
+                                  num_parallel_calls=tf.data.AUTOTUNE)
         options = tf.data.Options()
         options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
         dataset = dataset.with_options(options)
         if dataset_name != 'train':
             dataset = dataset.repeat(1)
         return dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
-
-    def read_image(self, sample):
-        sample['image'] = tf.cast(tf.io.decode_image(tf.io.read_file(sample['image_path']), channels=3), dtype=tf.float32)
-        return sample
 
     def augm(self, sample):
         image = tf.image.random_flip_up_down(image=sample['image'])
@@ -135,27 +139,35 @@ class MelData:
         image = tf.image.random_contrast(image=image, lower=.5, upper=1.5)
         image = tf.clip_by_value(image, clip_value_min=0., clip_value_max=255.)
         image = tf.image.random_saturation(image=image, lower=0.8, upper=1.2)
-        image = tfa.image.sharpness(image=image, factor=self.tf_rng.uniform(shape=[1], minval=0.5, maxval=1.5), name='Sharpness') # _sharpness_image ->     image_channels = tf.shape(image)[-1]
-        trans_val = self.args['input_shape'][0] * 0.2
-        image = tfa.image.translate(images=image, translations=self.tf_rng.uniform(shape=[2], minval=-trans_val, maxval=trans_val,
-                                                                                   dtype=tf.float32), name='Translation')
-        image = tfa.image.rotate(images=image, angles=tf.cast(self.tf_rng.uniform(shape=[], minval=0, maxval=360,
-                                                                                  dtype=tf.int32), dtype=tf.float32), interpolation='bilinear', name='Rotation')
-        image = tf.cond(tf.less(self.tf_rng.uniform(shape=[1]), 0.5),
-                        lambda: tfa.image.gaussian_filter2d(image=image, sigma=1.5, filter_shape=3, name='Gaussian_filter'),
+        image = tfa.image.sharpness(image=image,
+                                    factor=self.rng.uniform(shape=[1], minval=0.5, maxval=1.5),
+                                    name='Sharpness')  # _sharpness_image -> image_channels = tf.shape(image)[-1]
+        trans_val = self.args['image_size'] * 0.2
+        image = tfa.image.translate(images=image,
+                                    translations=self.rng.uniform(shape=[2], minval=-trans_val, maxval=trans_val,
+                                                                  dtype=tf.float32), name='Translation')
+        image = tfa.image.rotate(images=image,
+                                 angles=tf.cast(self.rng.uniform(shape=[], minval=0, maxval=360, dtype=tf.int32),
+                                                dtype=tf.float32),
+                                 interpolation='bilinear', name='Rotation')
+        image = tf.cond(tf.less(self.rng.uniform(shape=[1]), 0.5),
+                        lambda: tfa.image.gaussian_filter2d(image=image,
+                                                            sigma=1.5, filter_shape=3, name='Gaussian_filter'),
                         lambda: image)
 
         # sample['image'] = tf.map_fn(fn=_augm, elems=sample['image'], fn_output_signature=tf.float32)
         cutout_ratio = 0.15
         for i in range(3):
-            mask_height = tf.cast(self.tf_rng.uniform(shape=[], minval=0, maxval=self.args['input_shape'][0] * cutout_ratio),
+            mask_height = tf.cast(self.rng.uniform(shape=[], minval=0, maxval=self.args['image_size'] * cutout_ratio),
                                   dtype=tf.int32) * 2
-            mask_width = tf.cast(self.tf_rng.uniform(shape=[], minval=0, maxval=self.args['input_shape'][0] * cutout_ratio),
+            mask_width = tf.cast(self.rng.uniform(shape=[], minval=0, maxval=self.args['image_size'] * cutout_ratio),
                                  dtype=tf.int32) * 2
             image = tfa.image.random_cutout(image, mask_size=(mask_height, mask_width))
-        sample['image'] = {'xept': tf.keras.applications.xception.preprocess_input,
-                           'incept': tf.keras.applications.inception_v3.preprocess_input,
-                           'effnet0': tf.keras.applications.efficientnet.preprocess_input,
-                           'effnet1': tf.keras.applications.efficientnet.preprocess_input,
-                           'effnet6': tf.keras.applications.efficientnet.preprocess_input}[self.args['pretrained']](image)
+        sample['image'] = \
+            {'xept': tf.keras.applications.xception.preprocess_input,
+             'incept': tf.keras.applications.inception_v3.preprocess_input,
+             'effnet0': tf.keras.applications.efficientnet.preprocess_input,
+             'effnet1': tf.keras.applications.efficientnet.preprocess_input,
+             'effnet6': tf.keras.applications.efficientnet.preprocess_input
+             }[self.args['pretrained']](image)
         return sample
