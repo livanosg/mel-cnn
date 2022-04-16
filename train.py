ca5832867b0f6d9f5943992b5c1621a7de209eca
Giorgos Livanos <livanos.g@gmail.com>
2022-04-17 00:37:35 +0300

    minor

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 from features_def import TASK_CLASSES
 from data_prep import MelData
-from custom_metrics import gmean
+from custom_metrics import GeometricMean
 from custom_callbacks import EnrTensorboard  # , LaterCheckpoint, LaterReduceLROnPlateau
 from custom_losses import losses
 
@@ -23,14 +23,14 @@ def train_fn(args, dirs, model, strategy):
                      'adadelta': tf.keras.optimizers.Adadelta}[args['optimizer']]
         model.compile(loss=loss, optimizer=optimizer(learning_rate=args['learning_rate'] * args['gpus']),
                       metrics=[tfa.metrics.F1Score(num_classes=len(TASK_CLASSES[args['task']]),
-                                                   average='macro', name='f1_macro'),
-                               gmean])
+                                                   average='macro', name='f1'),
+                               GeometricMean(num_classes=len(TASK_CLASSES[args['task']]))])
 
     with open(dirs['model_summary'], 'w', encoding='utf-8') as model_summary:
         model.summary(print_fn=lambda x: model_summary.write(x + '\n'))
     train_data = data.get_dataset(dataset_name='train')
     val_data = data.get_dataset(dataset_name='validation')
-    callbacks = [tf.keras.callbacks.EarlyStopping(patience=args['early_stop'], verbose=1, monitor='val_gmean',
+    callbacks = [tf.keras.callbacks.EarlyStopping(patience=args['early_stop'], verbose=1, monitor='val_geometric_mean',
                                                   mode='max', restore_best_weights=True),
                  tf.keras.callbacks.CSVLogger(filename=dirs['train_logs'],
                                               separator=',', append=True),
