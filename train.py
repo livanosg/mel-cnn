a1f19f55166cbff92811364bcb1d049a40f7501a
Giorgos Livanos <livanos.g@gmail.com>
2022-05-11 01:34:52 +0300

    minor changes

--- train.py
+++ train.py
@@ -11,32 +11,27 @@ def train_fn(args: dict, dirs: dict, data, model, strategy):
     """Setup and run training stage"""
     loss = losses(args)[args['loss_fn']]
 
+    optimizer = {'adam': tf.keras.optimizers.Adam, 'adamax': tf.keras.optimizers.Adamax,
+                 'nadam': tf.keras.optimizers.Nadam, 'ftrl': tf.keras.optimizers.Ftrl,
+                 'rmsprop': tf.keras.optimizers.RMSprop, 'sgd': tf.keras.optimizers.SGD,
+                 'adagrad': tf.keras.optimizers.Adagrad, 'adadelta': tf.keras.optimizers.Adadelta
+                 }[args['optimizer']]
     with strategy.scope():
-        optimizer = {'adam': tf.keras.optimizers.Adam,
-                     'adamax': tf.keras.optimizers.Adamax,
-                     'nadam': tf.keras.optimizers.Nadam,
-                     'ftrl': tf.keras.optimizers.Ftrl,
-                     'rmsprop': tf.keras.optimizers.RMSprop,
-                     'sgd': tf.keras.optimizers.SGD,
-                     'adagrad': tf.keras.optimizers.Adagrad,
-                     'adadelta': tf.keras.optimizers.Adadelta}[args['optimizer']](learning_rate=args['learning_rate'] * args['gpus'])
-
-        model.compile(loss=loss, optimizer=optimizer,
-                      metrics=[tfa.metrics.F1Score(num_classes=len(TASK_CLASSES[args['task']]),
-                                                   average='macro', name='f1'),
+        model.compile(loss=loss, optimizer=optimizer(learning_rate=args['learning_rate'] * args['gpus']),
+                      metrics=[tfa.metrics.F1Score(num_classes=len(TASK_CLASSES[args['task']]), average='macro', name='f1'),
                                GeometricMean()])
+
     with redirect_stdout(open(dirs['model_summary'], 'w', encoding='utf-8')):
         model.summary(show_trainable=True)
 
-    model.fit(x=data.get_dataset('train'), validation_data=data.get_dataset('validation'), epochs=args['epochs'],
+    train_data = data.get_dataset('train')
+    val_data = data.get_dataset('validation')
+    model.fit(x=train_data, validation_data=val_data, epochs=args['epochs'],
               callbacks=[tf.keras.callbacks.CSVLogger(filename=dirs['train_logs'], separator=',', append=True),
                          tf.keras.callbacks.EarlyStopping(monitor='val_geometric_mean', mode='max', verbose=1,
                                                           patience=args['early_stop'], restore_best_weights=True),
-                         tf.keras.callbacks.CSVLogger(filename=dirs['train_logs'], separator=',', append=True),
-                         EnrTensorboard(val_data=data.get_dataset('validation'), log_dir=dirs['logs'],
+                         EnrTensorboard(val_data=val_data, log_dir=dirs['logs'],
                                         class_names=TASK_CLASSES[args['task']])
                          ]
               )
-    model.compile(optimizer=optimizer, loss=None)
     model.save(filepath=dirs['save_path'])
-    return model
