a03dc6226b4c5788987fc06fa33b764326c49cb6
Giorgos Livanos <livanos.g@gmail.com>
2022-05-11 01:30:34 +0300

    refactor callbacks

--- custom_callbacks.py
+++ custom_callbacks.py
@@ -1,48 +1,25 @@
 import numpy as np
 import tensorflow as tf
-from custom_metrics import calc_metrics, cm_image
+from custom_metrics import cm_image
 
 
 class EnrTensorboard(tf.keras.callbacks.TensorBoard):
     def __init__(self, val_data, class_names, **kwargs):
-        super().__init__(profile_batch=0, write_steps_per_second=True, **kwargs)
-        self.val_data = val_data
+        super().__init__(**kwargs)
         self.class_names = class_names
+        self.val_data = val_data
 
     def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
-        # logs["learning_rate"] = self.model.optimizer.lr
-
-        # Use the model to predict the values from the validation dataset.
-        y_true = []
-        test_pred = self.model.predict(self.val_data)
-        test_pred = np.argmax(test_pred, axis=-1)
-        for data in self.val_data:
-            y_true.append(np.argmax(data[1]["class"], axis=-1))
-        y_true = np.concatenate(y_true)
-        # Calculate the confusion matrix
-        confmat_image = cm_image(y_true=y_true, y_pred=test_pred, class_names=self.class_names)
-        image = tf.image.decode_png(confmat_image, channels=3)
-        image = tf.expand_dims(image, 0)
+        logs["learning_rate"] = self.model.optimizer.lr
         with super()._val_writer.as_default():
-            tf.summary.image("Confusion Matrix", image, step=epoch)
+            tf.summary.image("Confusion Matrix",
+                             tf.expand_dims(
+                                 tf.image.decode_png(cm_image(
+                                     y_true=np.argmax(np.concatenate(list(self.val_data.map(lambda samples, labels: labels['class']))), axis=-1),
+                                     y_pred=np.argmax(self.model.predict(self.val_data), axis=-1), class_names=self.class_names), channels=3), 0),
+                             step=epoch)
         super().on_epoch_end(epoch=epoch, logs=logs)
-
-
-class TestCallback(tf.keras.callbacks.Callback):
-    def __init__(self, args, validation, test, isic20_test):
-        super().__init__()
-        self.args = args
-        self.validation = validation
-        self.test = test
-        self.isic20_test = isic20_test
-
-    def on_train_end(self, logs=None):
-        model = tf.keras.models.load_model(self.args['dir_dict']['model_path'], compile=False)
-        calc_metrics(args=self.args, model=model, dataset=self.validation, dataset_name='validation')
-        calc_metrics(args=self.args, model=model, dataset=self.test, dataset_name='test')
-        if self.args['task'] in ('ben_mal', '5cls'):
-            calc_metrics(args=self.args, model=model, dataset=self.isic20_test, dataset_name='isic20_test')
 
 
 class LaterCheckpoint(tf.keras.callbacks.ModelCheckpoint):
