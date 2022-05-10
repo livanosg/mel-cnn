a97c91bd30a2f850435d90e43715fcf3cc7e923d
Giorgos Livanos <livanos.g@gmail.com>
2022-05-11 01:32:27 +0300

    re-write dataloader

--- data_prep.py
+++ data_prep.py
@@ -6,17 +6,14 @@ from features_def import BEN_MAL_MAP, LOCATIONS, IMAGE_TYPE, SEX, AGE_APPROX, TA
 from settings import data_csv
 
 
-def read_image(sample):
-    sample['image'] = tf.cast(x=tf.io.decode_image(tf.io.read_file(sample['image_path']), channels=3), dtype=tf.float32)
-    return sample
-
-
 class MelData:
     def __init__(self, args, dirs):
         self.args = args
         self.dirs = dirs
         self.rng = tf.random.Generator.from_non_deterministic_state()  # .from_seed(1312)
         self.class_names = TASK_CLASSES[self.args['task']]
+        self.dfs = {key: self._prep_df(key) for key in data_csv.keys()}
+        [self._log_info(mode=mode, df=self.dfs[mode]) for mode in ('train', 'validation', 'test')]
 
     def _log_info(self, mode, df):
         """log datasets general information"""
@@ -32,24 +29,7 @@ class MelData:
                     logs_df = logs_df[self.class_names]  # ?
                     logs_df.to_excel(writer, sheet_name=feature)
 
-    def _df_to_onehot_inputs_dict(self, df):
-        onehot_input_dict = {'image_path': df['image'].values}
-        onehot_feature_dict = {}
-        for key, voc in (('location', LOCATIONS), ('sex', SEX), ('age_approx', AGE_APPROX), ('image_type', IMAGE_TYPE)):
-            lookup = tf.keras.layers.StringLookup(vocabulary=voc, output_mode='one_hot')
-            onehot_feature_dict[key] = lookup(tf.convert_to_tensor(df[key].values))[:, 1:]
-        onehot_feature_dict['anatom_site_general'] = onehot_feature_dict['location']  # compat
-
-        if not self.args['no_clinical_data']:
-            onehot_input_dict['clinical_data'] = tf.keras.layers.Concatenate()([onehot_feature_dict['location'],
-                                                                                onehot_feature_dict['sex'],
-                                                                                onehot_feature_dict['age_approx']])
-            if not self.args['no_image_type']:
-                onehot_input_dict['clinical_data'] = tf.keras.layers.Concatenate()([onehot_input_dict['clinical_data'],
-                                                                                    onehot_feature_dict['image_type']])
-        return onehot_input_dict
-
-    def prep_df(self, mode: str):
+    def _prep_df(self, mode: str):
         df = pd.read_csv(data_csv[mode])
         if mode in ('train', 'validation'):
             df = df.sample(frac=self.args['dataset_frac'], random_state=1312)
@@ -78,10 +58,25 @@ class MelData:
         else:  # Keep dermoscopy or clinical image samples for the rest datasets according to training image type
             if self.args['image_type'] != 'both':
                 df.drop(df[~df['image_type'].isin([self.args['image_type']])].index, errors='ignore', inplace=True)
+        return df
 
-        self._log_info(mode=mode, df=df)
+    def _prep_df_for_tf_dataset(self, mode):
+        df = self.dfs[mode]
+        onehot_input_dict = {'image_path': df['image'].values}
+        onehot_feature_dict = {}
+        for key, voc in (('location', LOCATIONS), ('sex', SEX), ('age_approx', AGE_APPROX), ('image_type', IMAGE_TYPE)):
+            lookup = tf.keras.layers.StringLookup(vocabulary=voc, output_mode='one_hot')
+            onehot_feature_dict[key] = lookup(tf.convert_to_tensor(df[key].values))[:, 1:]
+        onehot_feature_dict['anatom_site_general'] = onehot_feature_dict['location']  # compat
 
-        onehot_input_dict = self._df_to_onehot_inputs_dict(df)
+        if not self.args['no_clinical_data']:
+            onehot_input_dict['clinical_data'] = tf.keras.layers.Concatenate()([onehot_feature_dict['location'],
+                                                                                onehot_feature_dict['sex'],
+                                                                                onehot_feature_dict['age_approx']])
+            if not self.args['no_image_type']:
+                onehot_input_dict['clinical_data'] = tf.keras.layers.Concatenate()([onehot_input_dict['clinical_data'],
+                                                                                    onehot_feature_dict['image_type']])
+        del onehot_feature_dict
         onehot_label = None
         sample_weight = None
 
@@ -110,64 +105,66 @@ class MelData:
                 sample_weight = tf.ones(onehot_label['class'].shape[0], dtype=tf.float32)
         return onehot_input_dict, onehot_label, sample_weight
 
+    def _read_image(self, sample):
+        sample['image'] = tf.cast(x=tf.io.decode_image(tf.io.read_file(sample['image_path']), channels=3),
+                                  dtype=tf.float32)
+        return sample
+
     def get_dataset(self, dataset=None):
-        ds = tf.data.Dataset.from_tensor_slices(self.prep_df(dataset))
-        if dataset == 'train':
-            ds = ds.shuffle(buffer_size=ds.cardinality(), reshuffle_each_iteration=True)
-        # Read image
-        ds = ds.map(lambda sample, label, sample_weights: (read_image(sample=sample), label, sample_weights),
-                    num_parallel_calls=16)
-        ds = ds.batch(self.args['batch_size'] * self.args['gpus'])  # Batch samples
-        if dataset == 'train':  # Apply image data augmentation on training dataset
-            ds = ds.map(lambda sample, label, sample_weights: (self.augm(sample), label, sample_weights),
-                        num_parallel_calls=16)
-        else:  # Remove sample_weights from validation and test datasets
-            ds = ds.map(lambda sample, label, sample_weights: (sample, label),
-                        num_parallel_calls=16)
-        if dataset == 'isic20_test':  # Remove label when isic20_test is running
-            ds = ds.map(lambda sample, label: sample,
-                        num_parallel_calls=16)
         options = tf.data.Options()
         options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
+        ds = tf.data.Dataset.from_tensor_slices(self._prep_df_for_tf_dataset(mode=dataset))
         ds = ds.with_options(options)
-        if dataset != 'train':
-            ds = ds.repeat(1)
-        return ds.prefetch(buffer_size=64)
+
+        # Memory leak due to shuffle: https://github.com/tensorflow/tensorflow/issues/44176#issuecomment-783768033
+        # if dataset == 'train':
+        #     buffer_size = ds.cardinality()
+        #     ds = ds.shuffle(buffer_size=buffer_size, seed=1312, reshuffle_each_iteration=True)
+        # if dataset != 'train':
+        # ds = ds.repeat(1)
+        # Read image
+        ds = ds.map(lambda sample, label, sample_weights: (self._read_image(sample=sample), label, sample_weights), num_parallel_calls=tf.data.AUTOTUNE)
+        ds = ds.batch(self.args['batch_size'] * self.args['gpus'])  # Batch samples
+
+        if dataset == 'train':  # Apply image data augmentation on training dataset
+            ds = ds.map(lambda sample, label, sample_weights: (self.augm(sample), label, sample_weights), num_parallel_calls=tf.data.AUTOTUNE)
+        elif dataset == 'isic20_test':  # Remove sample_weights from validation and test datasets
+            ds = ds.map(lambda sample, label, sample_weights: sample, num_parallel_calls=tf.data.AUTOTUNE)
+        else:
+            ds = ds.map(lambda sample, label, sample_weights: (sample, label), num_parallel_calls=tf.data.AUTOTUNE)
+        return ds.prefetch(buffer_size=10 * self.args['batch_size'] * self.args['gpus'])
 
     def augm(self, sample):
-        image = tf.image.random_flip_up_down(image=sample['image'])
-        image = tf.image.random_flip_left_right(image=image)
-        image = tf.image.random_brightness(image=image, max_delta=60.)
-        image = tf.image.random_contrast(image=image, lower=.5, upper=1.5)
-        image = tf.clip_by_value(image, clip_value_min=0., clip_value_max=255.)
-        image = tf.image.random_saturation(image=image, lower=0.8, upper=1.2)
-        image = tfa.image.sharpness(image=image,
-                                    factor=self.rng.uniform(shape=[1], minval=0.5, maxval=1.5),
-                                    name='Sharpness')  # _sharpness_image -> image_channels = tf.shape(image)[-1]
+        img = tf.image.random_flip_up_down(image=sample['image'])
+        img = tf.image.random_flip_left_right(image=img)
+        img = tf.image.random_brightness(image=img, max_delta=60.)
+        img = tf.image.random_contrast(image=img, lower=.5, upper=1.5)
+        img = tf.clip_by_value(img, clip_value_min=0., clip_value_max=255.)
+        img = tf.image.random_saturation(image=img, lower=0.8, upper=1.2)
+        img = tfa.image.sharpness(image=img, factor=self.rng.uniform(shape=[1], minval=0.5, maxval=1.5),
+                                  name='Sharpness')  # _sharpness_image -> image_channels = tf.shape(image)[-1]
         trans_val = self.args['image_size'] * 0.2
-        image = tfa.image.translate(images=image,
-                                    translations=self.rng.uniform(shape=[2], minval=-trans_val, maxval=trans_val,
-                                                                  dtype=tf.float32), name='Translation')
-        image = tfa.image.rotate(images=image,
-                                 angles=tf.cast(self.rng.uniform(shape=[], minval=0, maxval=360, dtype=tf.int32),
-                                                dtype=tf.float32),
-                                 interpolation='bilinear', name='Rotation')
-        image = tf.cond(tf.less(self.rng.uniform(shape=[1]), 0.5),
-                        lambda: tfa.image.gaussian_filter2d(image=image,
-                                                            sigma=1.5, filter_shape=3, name='Gaussian_filter'),
-                        lambda: image)
+        img = tfa.image.translate(images=img, translations=self.rng.uniform(shape=[2],
+                                                                            minval=-trans_val, maxval=trans_val,
+                                                                            dtype=tf.float32),
+                                  name='Translation')
+        img = tfa.image.rotate(images=img, angles=tf.cast(self.rng.uniform(shape=[],  dtype=tf.int32,
+                                                                           minval=0, maxval=360), dtype=tf.float32),
+                               interpolation='bilinear', name='Rotation')
+        img = tf.cond(tf.less(self.rng.uniform(shape=[1]), 0.5),
+                      lambda: tfa.image.gaussian_filter2d(image=img, sigma=1.5, filter_shape=3, name='Gaussian_filter'),
+                      lambda: img)
         cutout_ratio = 0.15
         for i in range(3):
             mask_height = tf.cast(self.rng.uniform(shape=[], minval=0, maxval=self.args['image_size'] * cutout_ratio),
                                   dtype=tf.int32) * 2
             mask_width = tf.cast(self.rng.uniform(shape=[], minval=0, maxval=self.args['image_size'] * cutout_ratio),
                                  dtype=tf.int32) * 2
-            image = tfa.image.random_cutout(image, mask_size=(mask_height, mask_width))
-        sample['image'] = \
-            {'xept': tf.keras.applications.xception.preprocess_input,
-             'incept': tf.keras.applications.inception_v3.preprocess_input,
-             'effnet0': tf.keras.applications.efficientnet.preprocess_input,
-             'effnet1': tf.keras.applications.efficientnet.preprocess_input,
-             'effnet6': tf.keras.applications.efficientnet.preprocess_input
-             }[self.args['pretrained']](image)
+            img = tfa.image.random_cutout(img, mask_size=(mask_height, mask_width))
+        sample['image'] = {'xept': tf.keras.applications.xception.preprocess_input,
+                           'incept': tf.keras.applications.inception_v3.preprocess_input,
+                           'effnet0': tf.keras.applications.efficientnet.preprocess_input,
+                           'effnet1': tf.keras.applications.efficientnet.preprocess_input,
+                           'effnet6': tf.keras.applications.efficientnet.preprocess_input
+                           }[self.args['pretrained']](img)
         return sample
--- models_init.py
+++ models_init.py
@@ -4,15 +4,11 @@ from features_def import TASK_CLASSES
 
 
 def model_struct(args):
-    conv_nodes = np.asarray([2, 3, 3.5, 4]) * args['conv_layers']
-    conv_nodes = conv_nodes.astype(np.int)
-    dense_nodes = np.asarray([1, 2]) * args['dense_layers']
-    merge_nodes = np.asarray([1, 2, 2.5]) * args['merge_layers']
-    merge_nodes = merge_nodes.astype(np.int)
-
-    act = {'swish': tf.keras.activations.swish,
-           'relu': tf.keras.activations.relu}[args['activation']]
-    rglzr = tf.keras.regularizers.l1_l2(l1=args['l1_reg'], l2=args['l1_reg'])
+    conv_nodes = np.multiply([2, 3, 3.5, 4], args['conv_layers']).astype(np.int)
+    dense_nodes = np.multiply([1, 2], args['dense_layers']).astype(np.int)
+    merge_nodes = np.multiply([1, 0.5, 0.25], args['merge_layers']) .astype(np.int)
+    act = {'swish': tf.keras.activations.swish, 'relu': tf.keras.activations.relu}[args['activation']]
+    l1_l2 = tf.keras.regularizers.l1_l2(l1=args['l1_reg'], l2=args['l1_reg'])
     seed = None
     inputs_list = []
     init = tf.keras.initializers.HeNormal()
@@ -46,21 +42,21 @@ def model_struct(args):
     # Inception module C used in Inception v4
     inc_avrg = tf.keras.layers.AveragePooling2D(padding='same', strides=1)(base_model)
     inc_avrg = conv2d_norm(input_tensor=inc_avrg, nodes=conv_nodes[0], activation=act, kernel_size=1,
-                           dropout=args['dropout'], kernel_regularizer=rglzr, initializer=init)
+                           dropout=args['dropout'], kernel_regularizer=l1_l2, initializer=init)
     inc_c1 = conv2d_norm(input_tensor=base_model, nodes=conv_nodes[0], activation=act, kernel_size=1,
-                         dropout=args['dropout'], kernel_regularizer=rglzr, initializer=init)
+                         dropout=args['dropout'], kernel_regularizer=l1_l2, initializer=init)
     inc_c2 = conv2d_norm(input_tensor=base_model, nodes=conv_nodes[1], activation=act, kernel_size=1,
-                         dropout=args['dropout'], kernel_regularizer=rglzr, initializer=init)
+                         dropout=args['dropout'], kernel_regularizer=l1_l2, initializer=init)
     inc_c2_1, inc_c2_2 = c_split(input_tensor=inc_c2, nodes=conv_nodes[0], activation=act,
-                                 dropout=args['dropout'], kernel_regularizer=rglzr, initializer=init)
+                                 dropout=args['dropout'], kernel_regularizer=l1_l2, initializer=init)
     inc_c3 = conv2d_norm(input_tensor=base_model, nodes=conv_nodes[1], activation=act, kernel_size=1,
-                         dropout=args['dropout'], kernel_regularizer=rglzr, initializer=init)
+                         dropout=args['dropout'], kernel_regularizer=l1_l2, initializer=init)
     inc_c3 = conv2d_norm(input_tensor=inc_c3, nodes=conv_nodes[2], activation=act, kernel_size=(1, 3),
-                         dropout=args['dropout'], kernel_regularizer=rglzr, initializer=init)
+                         dropout=args['dropout'], kernel_regularizer=l1_l2, initializer=init)
     inc_c3 = conv2d_norm(input_tensor=inc_c3, nodes=conv_nodes[3], activation=act, kernel_size=(3, 1),
-                         dropout=args['dropout'], kernel_regularizer=rglzr, initializer=init)
+                         dropout=args['dropout'], kernel_regularizer=l1_l2, initializer=init)
     inc_c3_1, inc_c3_2 = c_split(input_tensor=inc_c3, nodes=conv_nodes[0], activation=act,
-                                 dropout=args['dropout'], kernel_regularizer=rglzr, initializer=init)
+                                 dropout=args['dropout'], kernel_regularizer=l1_l2, initializer=init)
     common = tf.keras.layers.Concatenate()([base_model, inc_avrg, inc_c1, inc_c2_1, inc_c2_2, inc_c3_1, inc_c3_2])
     common = tf.keras.layers.Flatten()(common)
 # --------------------------------================ Tabular data =================--------------------------------- #
@@ -70,29 +66,29 @@ def model_struct(args):
             shape = (18,)
         clinical_data_input = tf.keras.layers.Input(shape=shape, name='clinical_data', dtype=tf.float32)
         inputs_list.append(clinical_data_input)
-        clinical_data_1 = tf.keras.layers.Dense(dense_nodes[1], activation=act, kernel_regularizer=rglzr, kernel_initializer=init, bias_initializer=init)(clinical_data_input)
+        clinical_data_1 = tf.keras.layers.Dense(dense_nodes[1], activation=act, kernel_regularizer=l1_l2, kernel_initializer=init, bias_initializer=init)(clinical_data_input)
         clinical_data_1 = tf.keras.layers.LayerNormalization()(clinical_data_1)
         clinical_data_1 = tf.keras.layers.Dropout(rate=args['dropout'], seed=seed)(clinical_data_1)
-        clinical_data_2 = tf.keras.layers.Dense(dense_nodes[0], activation=act, kernel_regularizer=rglzr, kernel_initializer=init, bias_initializer=init)(clinical_data_1)
+        clinical_data_2 = tf.keras.layers.Dense(dense_nodes[0], activation=act, kernel_regularizer=l1_l2, kernel_initializer=init, bias_initializer=init)(clinical_data_1)
         clinical_data_2 = tf.keras.layers.LayerNormalization()(clinical_data_2)
         clinical_data_2 = tf.keras.layers.Dropout(rate=args['dropout'], seed=seed)(clinical_data_2)
         clinical_data_con = tf.keras.layers.Concatenate(axis=-1)([clinical_data_2, clinical_data_1])
-        clinical_data_3 = tf.keras.layers.Dense(dense_nodes[0], activation=act, kernel_regularizer=rglzr, kernel_initializer=init, bias_initializer=init)(clinical_data_con)
+        clinical_data_3 = tf.keras.layers.Dense(dense_nodes[0], activation=act, kernel_regularizer=l1_l2, kernel_initializer=init, bias_initializer=init)(clinical_data_con)
         clinical_data_3 = tf.keras.layers.LayerNormalization()(clinical_data_3)
         clinical_data_3 = tf.keras.layers.Dropout(rate=args['dropout'], seed=seed)(clinical_data_3)
         common = tf.keras.layers.Concatenate(axis=-1)([common, clinical_data_1, clinical_data_2, clinical_data_3])
     # -------------------------------================== Concat part ==================---------------------------------#
     # common = tf.keras.layers.GlobalAvgPool2D()(common)
-    common = tf.keras.layers.Dense(merge_nodes[2], activation=act, kernel_regularizer=rglzr, kernel_initializer=init,
+    common = tf.keras.layers.Dense(merge_nodes[0], activation=act, kernel_regularizer=l1_l2, kernel_initializer=init,
                                    bias_initializer=init)(common)
     common = tf.keras.layers.LayerNormalization()(common)
     # common = tf.keras.layers.Dropout(rate=args['dropout'], seed=seed)(common)
-    common = tf.keras.layers.Dense(merge_nodes[1], activation=act, kernel_regularizer=rglzr, kernel_initializer=init,
+    common = tf.keras.layers.Dense(merge_nodes[1], activation=act, kernel_regularizer=l1_l2, kernel_initializer=init,
                                    bias_initializer=init)(common)
     common = tf.keras.layers.LayerNormalization()(common)
     # common = tf.keras.layers.Dropout(rate=args['dropout'], seed=seed)(common)
-    common = tf.keras.layers.Dense(merge_nodes[0], activation=act, kernel_regularizer=rglzr, kernel_initializer=init, bias_initializer=init)(common)
+    common = tf.keras.layers.Dense(merge_nodes[2], activation=act, kernel_regularizer=l1_l2, kernel_initializer=init, bias_initializer=init)(common)
     # common = tf.keras.layers.LayerNormalization()(common)
     # common = tf.keras.layers.Dense(16, activation=activation, kernel_regularizer=rglzr)(common)
-    output = tf.keras.layers.Dense(len(TASK_CLASSES[args['task']]), activation='softmax', kernel_regularizer=rglzr, kernel_initializer=init, bias_initializer=init, name='class')(common)
+    output = tf.keras.layers.Dense(len(TASK_CLASSES[args['task']]), activation='softmax', kernel_regularizer=l1_l2, kernel_initializer=init, bias_initializer=init, name='class')(common)
     return tf.keras.Model(inputs_list, [output])
