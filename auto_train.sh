a761564d58d6e4d8550d40158d0679c672a75e96
Giorgos Livanos <livanos.g@gmail.com>
2022-06-14 05:07:58 +0300

    set next runs

--- auto_train.sh
+++ auto_train.sh
@@ -14,6 +14,18 @@
 #python main.py -pt effnet6 -task nev_mel -it clinic -is 224 -btch 32 -opt adamax -loss focal -ws -cval -clrs 128 -dlrs 128 -mlrs 512 -l1 0. -l2 1e-7 -lr 1e-6 -dor 0.2 -fine -load /home/giorgos/projects/mel-cnn/models/nev_mel/clinic/010622060530
 #python main.py -pt effnet6 -task ben_mal -it clinic -is 224 -btch 32 -opt adamax -loss focal -ws -cval -clrs 128 -dlrs 128 -mlrs 512 -l1 0. -l2 1e-7 -lr 1e-6 -dor 0.2 -fine -load /home/giorgos/projects/mel-cnn/models/ben_mal/clinic/010622063249
 
-python main.py -pt effnet6 -task nev_mel -it both -is 224 -btch 64 -opt adamax -ncd -loss focal -ws -cval -clrs 128 -mlrs 512 -l1 0. -l2 1e-7 -lr 1e-5 -dor 0.2 # same as 170422170216
-python main.py -pt effnet6 -task nev_mel -it clinic -is 224 -btch 32 -opt adamax -ncd -loss focal -ws -cval -clrs 128 -mlrs 512 -l1 0. -l2 1e-7 -lr 1e-6 -dor 0.2 -fine -load /home/giorgos/projects/mel-cnn/models/nev_mel/both/020622023230 # fine tune above
+#python main.py -pt effnet6 -task nev_mel -it clinic -is 224 -btch 64 -ws -cval # all inputs done run 5x
 
+python main.py -pt effnet6 -task nev_mel -it clinic -ncd -is 224 -btch 64 -ws -cval # run x2 times
+
+python main.py -pt effnet6 -task ben_mal -it clinic -is 224 -btch 64 -ws -cval # all inputs
+python main.py -pt effnet6 -task ben_mal -it clinic -is 224 -btch 64 -ws -cval # all inputs
+python main.py -pt effnet6 -task ben_mal -it clinic -is 224 -btch 64 -ws -cval # all inputs
+python main.py -pt effnet6 -task ben_mal -it clinic -is 224 -btch 64 -ws -cval # all inputs
+
+python main.py -pt effnet6 -task ben_mal -it clinic -ncd -is 224 -btch 64 -ws -cval # only image
+python main.py -pt effnet6 -task ben_mal -it clinic -ncd -is 224 -btch 64 -ws -cval # only image
+python main.py -pt effnet6 -task ben_mal -it clinic -ncd -is 224 -btch 64 -ws -cval # only image
+python main.py -pt effnet6 -task ben_mal -it clinic -ncd -is 224 -btch 64 -ws -cval # only image
+
+#python main.py -pt effnet6 -task nev_mel -it clinic -is 224 -btch 32 -opt adamax -ncd -loss focal -ws -cval -clrs 128 -mlrs 512 -l1 0. -l2 1e-7 -lr 1e-6 -dor 0.2 -fine -load /home/giorgos/projects/mel-cnn/models/nev_mel/both/020622023230 # fine tune above
