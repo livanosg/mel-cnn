ce32192bfceb2f4ac0736d91400c870b7da0a4c6
Giorgos Livanos <livanos.g@gmail.com>
2022-10-10 12:49:33 +0300

    Revert "fix memory issues"
    This reverts commit c5c25dd7bdc65ff199acd529af3ce93720e54ba5.

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 from custom_metrics import calc_metrics
-from data_prep import get_dataset
+from data_prep import MelData
 
 
 def test_fn(args, dirs, model):
+    data = MelData(args, dirs)
 
     """ run validation and tests"""
     assert args['image_type'] in ('clinic', 'derm')
     thr_d, thr_f1 = calc_metrics(args=args, dirs=dirs, model=model,
-                                 dataset=get_dataset(args=args, dataset='validation', dirs=dirs),
+                                 dataset=data.get_dataset(dataset='validation'),
                                  dataset_name='validation')
     test_datasets = {'derm': ['isic16_test', 'isic17_test', 'isic18_val_test',
                               'mclass_derm_test', 'up_test'],
@@ -17,9 +18,9 @@ def test_fn(args, dirs, model):
 
     for test_dataset in test_datasets[args['image_type']]:
         calc_metrics(args=args, dirs=dirs, model=model,
-                     dataset=get_dataset(args=args, dataset=test_dataset, dirs=dirs),
+                     dataset=data.get_dataset(dataset=test_dataset),
                      dataset_name=test_dataset, dist_thresh=thr_d, f1_thresh=thr_f1)
     if args['task'] == 'ben_mal':
         calc_metrics(args=args, dirs=dirs, model=model,
-                     dataset=get_dataset(args=args, dataset='isic20_test', dirs=dirs),
+                     dataset=data.get_dataset(dataset='isic20_test'),
                      dataset_name='isic20_test', dist_thresh=thr_d, f1_thresh=thr_f1)
