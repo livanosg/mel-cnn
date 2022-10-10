5981ba46eb33d00691933b9f9f2d8f5dd2f705e6
Giorgos Livanos <livanos.g@gmail.com>
2022-10-10 12:49:06 +0300

    Revert "fix memory issues"
    This reverts commit 9b53d4489f16a802537de76e80b4fe8c8e5e1f30.

---
+++ setup_model.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from custom_losses import categorical_focal_loss
+from custom_metrics import GeometricMean
+from models_init import model_struct
+
+# tf.config.threading.set_inter_op_parallelism_threads(num_threads=16)
+# tf.config.threading.set_intra_op_parallelism_threads(num_threads=16)
+# tf.config.set_soft_device_placement(enabled=True)
+
+
+def unfreeze_model(trained_model):
+    """Make model trainable except BatchNormalization Layers"""
+    for layer in trained_model.layers:
+        if not isinstance(layer, tf.keras.layers.BatchNormalization):
+            layer.trainable = True
+    return trained_model
+
+
+def setup_model(args, dirs):
+    """Setup training strategy. Select one of mirrored or singlegpu.
+    Also check if a path to load a model is available and loads or setups a new model accordingly"""
+    cross_device_ops = tf.distribute.HierarchicalCopyAllReduce() if args['os'] == 'win32'\
+        else tf.distribute.NcclAllReduce()
+    strategy = tf.distribute.MirroredStrategy(cross_device_ops=cross_device_ops) if args['strategy'] == 'mirrored'\
+        else tf.distribute.OneDeviceStrategy('GPU')
+    assert args['gpus'] == strategy.num_replicas_in_sync
+    with strategy.scope():
+        if args['load_model']:
+            model = tf.keras.models.load_model(dirs['load_path'],
+                                               compile=True,
+                                               custom_objects={'categorical_focal_loss_fixed': categorical_focal_loss(),
+                                                               'GeometricMean': GeometricMean})
+        else:
+            model = model_struct(args=args)
+        if args['fine']:
+            model = unfreeze_model(model)
+        else:
+            for layer in model.layers:
+                if layer.name.startswith(('efficient', 'inception', 'xception')):
+                    layer.trainable = False
+    return model, strategy
---
+++ train.py
@@ -0,0 +1,43 @@
+from contextlib import redirect_stdout
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+# from custom_callbacks import EnrTensorboard, MemFix
+from data_prep import get_dataset
+from features_def import TASK_CLASSES
+from custom_metrics import GeometricMean
+from custom_losses import losses
+
+
+def train_fn(args: dict, dirs: dict, model, strategy):
+    """Setup and run training stage"""
+
+    with strategy.scope():
+        optimizer = {'adam': tf.keras.optimizers.Adam, 'adamax': tf.keras.optimizers.Adamax,
+                     'nadam': tf.keras.optimizers.Nadam, 'ftrl': tf.keras.optimizers.Ftrl,
+                     'rmsprop': tf.keras.optimizers.RMSprop, 'sgd': tf.keras.optimizers.SGD,
+                     'adagrad': tf.keras.optimizers.Adagrad, 'adadelta': tf.keras.optimizers.Adadelta
+                     }[args['optimizer']]
+
+        loss = losses(args)[args['loss_fn']]
+        train_data = get_dataset(args=args, dataset='train', dirs=dirs)
+        validation_data = get_dataset(args=args, dataset='validation', dirs=dirs)
+        model.compile(loss=loss, optimizer=optimizer(learning_rate=args['learning_rate'] * args['gpus']),
+                      metrics=[
+                          tfa.metrics.F1Score(num_classes=len(TASK_CLASSES[args['task']]), average='macro', name='f1'),
+                          GeometricMean()])
+
+    with redirect_stdout(open(dirs['model_summary'], 'w', encoding='utf-8')):
+        model.summary()  # show_trainable=True)
+
+    model.fit(x=train_data,
+              use_multiprocessing=False, workers=1,
+              validation_data=validation_data,
+              epochs=args['epochs'], verbose=2,
+              callbacks=[tf.keras.callbacks.CSVLogger(filename=dirs['train_logs'], separator=',', append=True),
+                         tf.keras.callbacks.EarlyStopping(monitor='val_geometric_mean', mode='max', verbose=1,
+                                                          patience=args['early_stop'], restore_best_weights=True),
+                         # EnrTensorboard(val_data=validation_data, log_dir=dirs['logs'], class_names=TASK_CLASSES[args['task']]),
+                         ]
+              )
+    model.save(filepath=dirs['save_path'])
